47e1bdd017e89d3ad7cde7a88c5c43efa5d1a14c
Luca Castellaro <luca.castellaro@signorprestito.it>
2023-08-10 08:46:05 +0200

    LC - Refactoring

--- generate-comuni-for-db/src/index.ts
+++ generate-comuni-for-db/src/index.ts
@@ -1,73 +1,19 @@
 import * as fs from 'fs/promises';
-
-type CrsProps = {
-    name: string
-}
-
-type Crs = {
-    type: string,
-    properties: CrsProps
-}
-
-type FeatureProps = {
-    name: string,
-    op_id: string,
-    minint_elettorale: string,
-    minint_finloc: string,
-    prov_name: string,
-    prov_istat_code: string,
-    prov_istat_code_num: number,
-    prov_acr: string,
-    reg_name: string,
-    reg_istat_code: string,
-    reg_istat_code_num: number,
-    opdm_id: string,
-    com_catasto_code: string,
-    com_istat_code: string,
-    com_istat_code_num: number
-}
-
-type Coordinates = number[][];
-
-type Geometry = {
-    type: string,
-    coordinates: Coordinates
-}
-
-type Feature = {
-    type: string,
-    properties: FeatureProps,
-    geometry: Geometry
-}
-
-type Data = {
-    type: string,
-    crs: Crs,
-    features: Feature[]
-};
-
-type ComuneForDb = {
-    Name: string,
-    Istat: number,
-    Fisco: string,
-    RegionIstat: number,
-    Province: string
-}
+import {Data, Feature, ComuneForDb} from './models/types';
 
 class Index {
     static async start():  Promise<any> {
-        const jsonData = await fs.readFile('../geojson/limits_IT_municipalities.geojson', 'utf-8');
+        const jsonData: string = await fs.readFile('../geojson/limits_IT_municipalities.geojson', 'utf-8');
 
         const data: Data = JSON.parse(jsonData);
-        const comuniSource: Feature[] = data.features;
-        const comuni = comuniSource
-            .map(xx => {
+        const comuni = data.features
+            .map(comune => {
                 return {
-                    Name: xx.properties.name,
-                    Istat: xx.properties.com_istat_code_num,
-                    Fisco: xx.properties.com_catasto_code,
-                    Province: xx.properties.prov_acr,
-                    RegionIstat: xx.properties.reg_istat_code_num
+                    Name: comune.properties.name,
+                    Istat: comune.properties.com_istat_code_num,
+                    Fisco: comune.properties.com_catasto_code,
+                    Province: comune.properties.prov_acr,
+                    RegionIstat: comune.properties.reg_istat_code_num
                 } as ComuneForDb;
             });
 
---
+++ generate-comuni-for-db/src/models/types.ts
@@ -0,0 +1,53 @@
+export type CrsProps = {
+    name: string
+}
+
+export type Crs = {
+    type: string,
+    properties: CrsProps
+}
+
+export type FeatureProps = {
+    name: string,
+    op_id: string,
+    minint_elettorale: string,
+    minint_finloc: string,
+    prov_name: string,
+    prov_istat_code: string,
+    prov_istat_code_num: number,
+    prov_acr: string,
+    reg_name: string,
+    reg_istat_code: string,
+    reg_istat_code_num: number,
+    opdm_id: string,
+    com_catasto_code: string,
+    com_istat_code: string,
+    com_istat_code_num: number
+}
+
+export type Coordinates = number[][];
+
+export type Geometry = {
+    type: string,
+    coordinates: Coordinates
+}
+
+export type Feature = {
+    type: string,
+    properties: FeatureProps,
+    geometry: Geometry
+}
+
+export type Data = {
+    type: string,
+    crs: Crs,
+    features: Feature[]
+};
+
+export type ComuneForDb = {
+    Name: string,
+    Istat: number,
+    Fisco: string,
+    RegionIstat: number,
+    Province: string
+}
